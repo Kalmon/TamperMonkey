ac804fdc33b913d447b595db2a793d84838f2121
KVT <kalmonvaladao@gmail.com>
2022-12-27 16:13:28 -0300

    Update MangaHost.js

--- MangaHost.js
+++ MangaHost.js
@@ -1,14 +1,12 @@
 // ==UserScript==
 // @name         Fav Host
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://mangahosted.com/*
 // @require https://code.jquery.com/jquery-3.6.0.min.js
 // @icon         https://i.pinimg.com/236x/b8/a9/6a/b8a96a8026d59b815cef59f98b8c48ec.jpg
-// @downloadURL  https://raw.githubusercontent.com/Kalmon/TamperMonkey/main/MangaHost.js
-// @updateURL    https://raw.githubusercontent.com/Kalmon/TamperMonkey/main/MangaHost.js
 // ==/UserScript==
 
 $("body").append(`
