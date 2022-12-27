022e1824356ac7051ba9f5b0e503f3962ca6fbec
KVT <kalmonvaladao@gmail.com>
2022-12-27 08:15:50 -0300

    Update MangaHost.js

--- MangaHost.js
+++ MangaHost.js
@@ -1,1 +1,21 @@
 
+let Scripts = ['https://unpkg.com/vue'];
+let temp;
+for(let cont=0;cont<Scripts.length;cont++){
+    if(Scripts[cont].includes(".css")){
+        $('head').append('<link rel="stylesheet" href="'+Scripts[cont]+'" type="text/css" />');
+    }else{
+        $('head').append('<link rel="stylesheet" href="'+Scripts[cont]+'" type="text/css" />');
+    }
+}
+(async function() {
+    'use strict';
+})();
+
+async function loadScript(src) {
+    return new Promise((resolv, reject) => {
+        fetch(src).then(response => response.text()).then(js => {
+            resolv(js);
+        })
+    });
+}
