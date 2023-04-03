8a6e0dca07f6226af9edab898835a8f5a4bd4e55
KVT <kalmonvaladao@gmail.com>
2023-04-03 09:09:21 -0300

    Update Juscelino_CNPJ.user.js

--- Juscelino_CNPJ.user.js
+++ Juscelino_CNPJ.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Juscelino - CNPJ
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/*
